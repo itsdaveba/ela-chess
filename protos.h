fb7c82c8ac8fa97f179c53fc75506a7daa43563a
Dave Barragan <itsdaveba@gmail.com>
2022-09-14 20:00:36 -0500

    prototypes

--- protos.h
+++ protos.h
@@ -1,7 +1,15 @@
+// main.c
+char *move_to_lan(move_t move);
+move_t lan_to_move(char *lan);
+
 // board.c
 bool set_board(char *fen);
 char *get_fen();
 void print_board();
 void add_move(int from, int to, int type);
 void gen_moves();
-char *move_to_lan(move_t move);
+bool attack(int square, int side);
+bool in_check(int side);
+bool make_move(move_t move);
+void take_back();
+u64 perft(int depth);
