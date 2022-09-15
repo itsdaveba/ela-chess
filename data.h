ecc08a4ae16a1e800b295ea3c62f1f426199d601
Dave Barragan <itsdaveba@gmail.com>
2022-09-14 19:53:29 -0500

    data header

--- data.h
+++ data.h
@@ -1,5 +1,9 @@
-extern int n_moves;
-extern move_t move_list[MAX_GEN_MOVES];
+extern int ply;
+extern int hply;
+extern hist_t history[MAX_HPLY];
+
+extern int n_moves[MAX_DEPTH];
+extern move_t move_list[MAX_DEPTH][MAX_GEN_MOVES];
 
 extern int piece[64];
 extern int color[64];
@@ -9,12 +13,13 @@ extern int passant;
 extern int halfmove;
 extern int fullmove;
 
-extern char piece_to_char[7];
-extern char castling_char[4];
-extern int board[64];
+extern const char piece_to_char[7];
+extern const char castling_char[4];
+extern const int board[64];
+extern const int castling_rights[64];
 
-extern int slider[7];
-extern int n_directions[7];
-extern int direction[7][8];
-extern int mailbox[120];
-extern int mailbox64[64];
+extern const int slider[7];
+extern const int n_directions[7];
+extern const int direction[7][8];
+extern const int mailbox[120];
+extern const int mailbox64[64];
