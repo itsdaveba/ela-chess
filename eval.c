6d3e6122e1943a93f9bce4d77d43025f225ed622
Dave Barragan <itsdaveba@gmail.com>
2022-09-18 11:08:00 -0500

    evaluation

---
+++ eval.c
@@ -0,0 +1,29 @@
+#include "defs.h"
+#include "data.h"
+
+int evaluate()
+{
+    int value = 0;
+
+    for (int s = 0; s < 64; s++)
+    {
+        if (color[s] == WHITE)
+        {
+            if (piece[s] != KING)
+            {
+                value += piece_value[piece[s]];
+            }
+            value += piece_table[piece[s]][s];
+        }
+        else if (color[s] == BLACK)
+        {
+            if (piece[s] != KING)
+            {
+                value -= piece_value[piece[s]];
+            }
+            value -= piece_table[piece[s]][bigend_rank[s]];
+        }
+    }
+
+    return side * value;
+}
