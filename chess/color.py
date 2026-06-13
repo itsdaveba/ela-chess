a849f00a09220ce18426d67673ada1f429762e7c
Dave Barragan <itsdaveba@gmail.com>
2026-06-13 17:56:56 +0100

    color

---
+++ chess/color.py
@@ -0,0 +1,25 @@
+
+
+class Color:
+    def __init__(self, val: bool | str) -> None:
+        self.white: bool
+
+        if isinstance(val, bool):
+            self.white = val
+            return
+
+        if len(val) != 1 or val not in "wb":
+            raise ValueError(f"invalid color char: {val}")
+
+        self.white = val == 'w'
+
+    def __repr__(self) -> str:
+        return f"Color.{self.name}"
+
+    @property
+    def char(self) -> str:
+        return "w" if self.white else "b"
+
+    @property
+    def name(self) -> str:
+        return "WHITE" if self.white else "BLACK"
---
+++ tests/test_color.py
@@ -0,0 +1,20 @@
+import pytest
+
+from chess import Color
+
+
+def test_color():
+    with pytest.raises(ValueError, match="invalid color char"):
+        Color('x')
+
+    color = Color(True)
+    assert repr(color) == "Color.WHITE"
+    assert color.white
+    assert color.char == 'w'
+    assert color.name == "WHITE"
+
+    color = Color('b')
+    assert repr(color) == "Color.BLACK"
+    assert not color.white
+    assert color.char == 'b'
+    assert color.name == "BLACK"
