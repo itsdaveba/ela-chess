eef5e62e4f60e7dcf4eba7d765080a8dfd6e2342
Dave Barragan <itsdaveba@gmail.com>
2026-06-27 20:58:19 +0100

    position dir

--- chess/__init__.py
+++ chess/__init__.py
@@ -1,7 +1,7 @@
-from .representation.color import Color
-from .representation.piece import Piece
-from .representation.board import Board
-from .representation.position import Position
+from .position.color import Color
+from .position.piece import Piece
+from .position.board import Board
+from .position.position import Position
 
 
 __all__ = ["Color", "Piece", "Board", "Position"]
