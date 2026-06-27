3da1bdb626e5dc5646963d5ebe1566a568c5b0ee
Dave Barragan <itsdaveba@gmail.com>
2026-06-27 20:44:28 +0100

    color char

--- chess/representation/color.py
+++ chess/representation/color.py
@@ -4,3 +4,15 @@ from enum import Enum
 class Color(int, Enum):
     WHITE = 0
     BLACK = 1
+
+    @classmethod
+    def from_char(cls, char: str) -> "Color":
+        if char == "w":
+            return Color.WHITE
+        elif char == "b":
+            return Color.BLACK
+        raise ValueError(f"invalid color char: '{char}'")
+
+    @property
+    def char(self) -> str:
+        return "wb"[self]
--- tests/test_color.py
+++ tests/test_color.py
@@ -1,6 +1,16 @@
+import pytest
+
 from chess import Color
 
 
 def test_color():
     assert len(Color) == 2
-    assert Color.WHITE != Color.BLACK
+
+    assert Color.WHITE.char == "w"
+    assert Color.BLACK.char == "b"
+
+    with pytest.raises(ValueError, match="invalid color char"):
+        Color.from_char("-")
+
+    assert Color.from_char("w") == Color.WHITE
+    assert Color.from_char("b") == Color.BLACK
