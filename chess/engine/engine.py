4dc8533a46dc3114eab635060a9fa1af0fde5530
Dave Barragan <itsdaveba@gmail.com>
2026-08-10 10:13:47 +0100

    init branch

--- chess/engine/engine.py
+++ chess/engine/engine.py
@@ -170,7 +170,7 @@ class EnginePlayer(Player):
             return 0
 
         if depth == 0:
-            return game.eval if game.side == Color.WHITE else -game.eval
+            return self.quiescence(game)
 
         key = game.hash % self.num_entries
         ttentry = self.tt[key]
@@ -208,3 +208,6 @@ class EnginePlayer(Player):
             self.tt[key] = TTEntry(game.hash, best_move, depth, best_score, type)
 
         return best_score
+
+    def quiescense(self, game: "ChessGame"):
+        return game.eval if game.side == Color.WHITE else -game.eval
